5ece7c1a6ab5d61128ba58665112c7575cb34a55
dhwajhome26-rgb <dhwajhome26@gmail.com>
2026-02-19 18:46:48 +0530

    Update vm.sh

--- vm.sh
+++ vm.sh
@@ -1,15 +1,20 @@
-#!/bin/bash
+#!/usr/bin/env bash
+set -e
 
 clear
 
 echo "Loading DhwaJXD VM Manager..."
 
-TMP_FILE="/tmp/.vm-core.sh"
+TMP_FILE="/tmp/.vm-core.$$"
 
+# Download core
 curl -fsSL https://raw.githubusercontent.com/dhwajhome26-rgb/VM/main/vm-core.sh -o "$TMP_FILE"
 
+# Make executable
 chmod +x "$TMP_FILE"
 
-bash "$TMP_FILE"
+# Run core
+"$TMP_FILE"
 
+# Cleanup
 rm -f "$TMP_FILE"
